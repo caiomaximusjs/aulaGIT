43599fc54c97df861eb862b2b2012cf5a266fd1e
caiomaximusjs <caiomaximus@hotmail.com>
2022-05-27 22:34:20 -0300

    segundo

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 let message = "";
+require("dotenv").config();
 const express = require('express')
 const app = express()
 const path = require("path");
+const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded());
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));// Dizendo ao express a pasta que irá guardar esses arquivos
+
 
 app.get('/', function (req, res) { // Chamando a rota Get, dentro do servidor que tem as rotas que o cliente está acessando o que chamamaos de endpoints, o cliente faz a requisição e acessa o endpoint que é uma rota e dentro dessa rota é executada uma função e responde o meu cliente 
   const devList = ["Backend", "Frontend", "Fullstack"];
@@ -20,4 +23,4 @@ app.post("/subscription", (req, res) => {
   res.redirect("/")
 })
 
-app.listen(3000, console.log("servidor rodando em http://localhost:3000"));
+app.listen(3000, console.log(`servidor rodando em http://localhost:${port}`));
